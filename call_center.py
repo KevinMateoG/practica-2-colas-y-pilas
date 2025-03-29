ce37e5bbf7e1901abfb7bdb32944f0657bc1226f
Kevin Mateo G <gomezdiezkevinmateo@gmail.com>
2025-03-29 13:44:36 -0500

    implementacion de los agentes y su respuesta con los agentes

--- call_center.py
+++ call_center.py
@@ -6,28 +6,28 @@ from random import randint
 lista_de_experiencia = ["novato", "intermedio", "experto"]
 
 class Agentes:
-    def __init__(self, mensaje):
-        self.id: int = id
+    def __init__(self):
+        self.id: int = randint(1, 400)
         self.nivel_experiencia: str = lista_de_experiencia[randint(0,2)]
         self.estado: str = "disponible"
-        self.tiempo_respuesta: int = self.calcular_tiempo_respuesta(mensaje)
+#        self.tiempo_respuesta: int = self.calcular_tiempo_respuesta(mensaje)
 
     def calcular_tiempo_respuesta(self, mensaje_recibido):
         tiempo_estimando = (len(mensaje_recibido.mensaje) / 10) + (mensaje_recibido.prioridad / 2)
 
         if self.nivel_experiencia == "novato":
             tiempo_respuesta = tiempo_estimando
-            self.estado = "ocupado"
         
         elif self.nivel_experiencia == "intermedio":
             tiempo_respuesta = tiempo_estimando*0.25
-            self.estado = "ocupado"
         
         elif self.nivel_experiencia == "experto":
             tiempo_respuesta = tiempo_estimando*0.5
-            self.estado = "ocupado"
-
+        
         return tiempo_respuesta
+    
+    def __repr__(self):
+        return str(self.id)
 
 class Mensaje:
     
@@ -71,7 +71,7 @@ def leer_mensaje():
             lista_mensajes.append(contenido)
     return lista_mensajes
 
-def agregar_a_cola():
+def agregar_a_cola() -> PriorityQueue:
     mensajes_a_recibir = leer_mensaje()
     crear_cola = PriorityQueue(orden)
     for mensaje in mensajes_a_recibir:
@@ -79,13 +79,39 @@ def agregar_a_cola():
         crear_cola.enqueue(crear_objeto_mensaje)
     return crear_cola
 
-ingresar_mensajes(
-    "tengo una duda mesale un mensaje con una ventana que dice error", "fallos"
-)
+def crear_agentes() -> list[Agentes]:
+    lista_de_agentes = []
+    for i in range(len(cola_con_mensaje)):
+        agente = Agentes()
+        lista_de_agentes.append(agente)
+    return lista_de_agentes
+
+def agente_con_mensaje(cola: PriorityQueue):
+    nueva_cola = PriorityQueue(orden)
+    for agente in lista_agentes:
+        if agente.estado == "disponible":
+            mensaje = cola.dequeue()
+            nueva_cola.enqueue(mensaje)
+            timepo_de_espera = agente.calcular_tiempo_respuesta(mensaje)
+            agente.estado = "ocupado"
+            print("---------------------")
+            sleep(timepo_de_espera)
+            print("PROBLEMA SOLUCIONADO")
+        agente.estado = "disponible"
+    
+    for _ in range(len(nueva_cola)):
+        cola.enqueue(nueva_cola.dequeue())
+    print("---------------------")
 
 orden = "max"
-cola = agregar_a_cola()
-agente_1 = Agentes(cola.first())
-print(agente_1.tiempo_respuesta)
-sleep(agente_1.tiempo_respuesta)
-print("esta listo")
+while True:
+    requiere_mensaje = input("¿Quieres agregar un nuevo mensaje?, si deseas salir pon la palabra SALIDA: ")
+    if requiere_mensaje.lower() == "si":
+        texto_del_mensaje = input("¿Que mensaje deseas ingresar?: ")
+        nombre_mensaje = input("Pon un titulo al mensaje: ")
+        ingresar_mensajes(texto_del_mensaje, nombre_mensaje)
+    if requiere_mensaje.lower() == "salida":
+        break
+    cola_con_mensaje = agregar_a_cola()
+    lista_agentes = crear_agentes()
+    agente_con_mensaje(cola_con_mensaje)
